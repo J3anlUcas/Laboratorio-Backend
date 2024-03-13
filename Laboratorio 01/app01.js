fcadaf88bb26c94fc7b86591617543a6618e07a2
JEAN LUCAS <jeanlucas2005@hotmail.com>
2024-03-13 09:35:54 -0300

    Primeiro commit - istalando o express e o node

---
+++ Laboratorio 01/app01.js
@@ -0,0 +1,19 @@
+const { Console } = require("console");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/cadastro", (req, res) => {
+    var vet = req.body;
+    console.log(vet);
+
+    for (let i = 0; i < vet.length; i++) {
+        var result = vet[i].nota >= 5 ? "Você passou" : "Reprovou animal"
+        console.log(`nome:${vet[i].nome} nota:${vet[i].nota} situação:${result}`)
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Servidor iniciado na porta 3001!")
+})
