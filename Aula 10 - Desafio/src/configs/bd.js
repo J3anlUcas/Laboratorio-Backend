fe21f7668a393097b596428d8815fd7d083ce278
Jean Lucas Oliveira Damaceno <160613430+J3anlUcas@users.noreply.github.com>
2024-04-01 22:49:47 -0300

    01-04

---
+++ Aula 10 - Desafio/src/configs/bd.js
@@ -0,0 +1,12 @@
+const dbConfig = {
+    server: "localhost",
+    database: "Laboratorio",
+    user: "sa",
+    password: "50272751",
+    port: 1433,
+    options: {
+        encrypt: false
+    }
+}
+
+module.exports = dbConfig
